3af43ae745e7e3ce83665b6e0b2aaede05bbd50f
gkhabada <gadzhidadaev95@gmail.com>
2022-10-04 16:39:25 +0300

    feat: add base in vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import eslint from 'vite-plugin-eslint'
 import StylelintPlugin from 'vite-plugin-stylelint';
 
 export default defineConfig({
+base: './',
+
   plugins: [
     eslint(),
     StylelintPlugin({}),
